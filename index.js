4c47f72b130b31263405dfbe6630c0d25397dc35
swapnil <swapnildixit515@gmail.com>
2022-09-12 23:25:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI);
+// const users = require("./routes/user");
+
+// app.use("/user", users);
+// app.use("/saving", require('./routes/saving'));
+// app.use("/posts", require('./routes/expense'));
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
+});
